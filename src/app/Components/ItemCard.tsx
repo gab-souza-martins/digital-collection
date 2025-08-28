545ce8992e452751c982f0d6f0e7e391d761da9c
Grey-B <gabriellemener@gmail.com>
2025-08-28 12:41:43 -0300

    Criado card básico de item da coleção para começar

---
+++ src/app/Components/ItemCard.tsx
@@ -0,0 +1,11 @@
+const ItemCard = () => {
+   return (
+      <div className="w-2xs border rounded-lg p-4 shadow-md">
+         <h2 className="text-2xl font-semibold">Preto perolado</h2>
+         <hr className="opacity-40" />
+         <p>Meu primeiro set de dados.</p>
+      </div>
+   );
+};
+
+export default ItemCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import ItemCard from "./Components/ItemCard";
+
 export default function Home() {
    return (
-      <div className="container p-4">
-         <h1 className="text-3xl font-bold">Coleção digital</h1>
+      <div className="p-4">
+         <h1 className="mb-6 text-3xl font-bold">Coleção digital</h1>
+         <ItemCard />
       </div>
    );
 }
