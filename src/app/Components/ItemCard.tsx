1b82094bf093f900ce70b4b8a3c4a503dbafc020
Grey-B <gabriellemener@gmail.com>
2025-08-29 14:20:39 -0300

    ItemCard agora mosta a data em que foi adicionado

--- src/app/Components/ItemCard.tsx
+++ src/app/Components/ItemCard.tsx
@@ -3,11 +3,16 @@ interface ItemCardProps {
 }
 
 const ItemCard: React.FC<ItemCardProps> = ({ title }) => {
+   const date: Date = new Date();
+
    return (
       <div className="w-2xs border rounded-lg p-4 shadow-md">
          <h2 className="text-2xl font-semibold">{title}</h2>
          <hr className="opacity-40" />
          <p>Descrição aqui.</p>
+         <p className="text-sm text-gray-500 mt-2">
+            Adicionado em {date.toLocaleDateString("pt-BR")}
+         </p>
       </div>
    );
 };
