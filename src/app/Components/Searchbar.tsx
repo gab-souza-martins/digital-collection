394c33299cefa25f93cfd9a73e0ea1e1ade3765b
Grey-B <gabriellemener@gmail.com>
2025-08-30 14:40:14 -0300

    Criado checkbox para filtrar por imagem em Searchbar.tsx, ainda sem funcionalidade

--- src/app/Components/Searchbar.tsx
+++ src/app/Components/Searchbar.tsx
@@ -10,17 +10,33 @@ const Searchbar: React.FC<SearchbarProps> = ({ textSearch }) => {
 
    return (
       <div>
-         <form className="flex items-center gap-1">
+         <form className="flex flex-col items-start gap-2 xs:flex-row xs:items-center xs:gap-3">
             <input
                onChange={(e) => {
                   setTextTerm(e.target.value);
                   textSearch(e.target.value);
                }}
                type="text"
-               className="border border-gray-800 rounded-md py-1 px-2"
+               className="border border-gray-800 rounded-md "
                value={textTerm}
                placeholder="Pesquisar itens"
+               aria-label="Pesquisar itens"
             />
+
+            <div className="flex items-center gap-1">
+               <label
+                  htmlFor="imageFilter"
+                  className="cursor-pointer text-gray-700"
+               >
+                  Possui imagem
+               </label>
+               <input
+                  type="checkbox"
+                  id="imageFilter"
+                  className="cursor-pointer border border-gray-800 rounded-md w-5 h-5"
+                  aria-label="Filtrar por imagem"
+               />
+            </div>
          </form>
       </div>
    );
