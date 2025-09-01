bb23362f5a505312e1b655661cf33abb09c76c6c
Grey-B <gabriellemener@gmail.com>
2025-09-01 14:53:26 -0300

    Alterado tamanho dos inputs de Searchbar.tsx para encaixar melhor em telas menores

--- src/app/Components/Searchbar.tsx
+++ src/app/Components/Searchbar.tsx
@@ -11,14 +11,14 @@ const Searchbar: React.FC<SearchbarProps> = ({ textSearch, imageFilter }) => {
 
    return (
       <div>
-         <form className="flex flex-col items-start gap-2 xs:flex-row xs:items-center xs:gap-3">
+         <form className="flex flex-row items-center gap-2 xs:gap-3">
             <input
                onChange={(e) => {
                   setTextTerm(e.target.value);
                   textSearch(e.target.value);
                }}
                type="text"
-               className="border border-gray-800 rounded-md py-1 px-2"
+               className="border border-gray-800 rounded-md py-1 px-2 w-40 xs:w-60"
                value={textTerm}
                placeholder="Pesquisar itens"
                aria-label="Pesquisar itens"
@@ -29,7 +29,7 @@ const Searchbar: React.FC<SearchbarProps> = ({ textSearch, imageFilter }) => {
                   htmlFor="imageFilter"
                   className="cursor-pointer text-gray-700"
                >
-                  Possui imagem
+                  Tem imagem
                </label>
                <input
                   onChange={(e) => imageFilter(e.target.checked)}
