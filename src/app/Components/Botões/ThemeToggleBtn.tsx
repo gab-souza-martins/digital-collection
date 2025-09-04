12232a5f24280a4ff38d18ab3c3e989f85647c5e
Grey-B <gabriellemener@gmail.com>
2025-09-04 13:36:54 -0300

    ThemeToggleBtn.tsx agora corretamente salva o theme no localStorage

--- src/app/Components/Botões/ThemeToggleBtn.tsx
+++ src/app/Components/Botões/ThemeToggleBtn.tsx
@@ -3,39 +3,54 @@ import React from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 const ThemeToggleBtn = () => {
-   const [toggled, setToggled] = React.useState<boolean>(false);
+   const [theme, setTheme] = React.useState<string>("light");
+
+   React.useEffect(() => {
+      const savedTheme = localStorage.getItem("theme");
+      const parsed = savedTheme ? JSON.parse(savedTheme) : "light";
+
+      if (parsed) {
+         document.querySelector("body")?.setAttribute("data-theme", parsed);
+      }
+   }, []);
+
+   const handleThemeChange = (t: string) => {
+      setTheme(t);
+      document.querySelector("body")?.setAttribute("data-theme", t);
+      localStorage.setItem("theme", JSON.stringify(t));
+   };
 
    const handleToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setToggled(!toggled);
       if (e.target.checked) {
-         document.querySelector("body")?.setAttribute("data-theme", "dark");
+         handleThemeChange("dark");
       } else {
-         document.querySelector("body")?.setAttribute("data-theme", "light");
+         handleThemeChange("light");
       }
    };
 
    return (
       <label
          htmlFor="themeToggle"
-         className={`relative cursor-pointer inline-block w-16 h-8 rounded-4xl transition duration-300 ease-in-out ${
-            toggled ? "bg-gray-800" : "bg-gray-500"
-         }`}
+         className="relative cursor-pointer inline-block w-16 h-8 rounded-4xl transition duration-300 ease-in-out"
+         style={{ backgroundColor: "var(--light-foreground)" }}
       >
          <span className="hidden">Mudar tema</span>
          <input
             type="checkbox"
             id="themeToggle"
             className="size-0 opacity-0"
-            defaultChecked={toggled}
+            defaultChecked={theme === "dark"}
             onChange={handleToggle}
          />
          <div
             className={`cursor-pointer absolute flex items-center justify-center top-1 left-1 size-6 rounded-full 
                 transition duration-300 ease-in-out text-gray-700 ${
-                   toggled ? "translate-x-8 bg-gray-600 text-white" : "bg-white"
+                   theme === "dark"
+                      ? "translate-x-8 bg-gray-600 text-white"
+                      : "bg-white"
                 }`}
          >
-            {toggled ? <FaMoon /> : <FaSun className="-ml-0.45" />}
+            {theme === "dark" ? <FaMoon /> : <FaSun className="-ml-0.45" />}
          </div>
       </label>
    );
