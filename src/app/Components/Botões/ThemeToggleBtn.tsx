bdf15a9d67820c29b78e9e9729c66bcfd90a94fb
Grey-B <gabriellemener@gmail.com>
2025-09-04 11:00:11 -0300

    Adicionado estilo e animação para ThemeToggleBtn.tsx

--- src/app/Components/Botões/ThemeToggleBtn.tsx
+++ src/app/Components/Botões/ThemeToggleBtn.tsx
@@ -9,14 +9,24 @@ const ThemeToggleBtn = () => {
    };
 
    return (
-      <label htmlFor="themeToggle">
-         Tema
+      <label htmlFor="themeToggle" className="relative inline-block w-16 h-8">
+         <span className="hidden">Mudar tema</span>
          <input
             type="checkbox"
             id="themeToggle"
+            className="size-0 opacity-0"
             defaultChecked={toggled}
             onClick={handleToggle}
          />
+         <span
+            className={`cursor-pointer absolute top-0 left-0 bottom-0 right-0 rounded-4xl transition duration-200
+                       before:absolute before:size-6 before:top-1 before:left-1 before:bg-white before:rounded-full 
+                       before:transition before:duration-300 ${
+                          toggled
+                             ? "bg-emerald-500 before:translate-x-8"
+                             : "bg-gray-500"
+                       }`}
+         ></span>
       </label>
    );
 };
