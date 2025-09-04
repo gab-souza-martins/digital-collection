b039dea2eef72f2b2f6a8199293cde3170c80809
Grey-B <gabriellemener@gmail.com>
2025-09-04 13:47:40 -0300

    Adicionada margem inferior ao rodapé para evitar que OpenAddFormBtn.tsx ficasse em cima dele

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
    return (
-      <p className="text-center text-sm">
+      <p className="text-center text-sm mb-13 xs:mb-0">
          2025 &copy; Gabriel de Souza - Todos os direitos reservados
       </p>
    );
